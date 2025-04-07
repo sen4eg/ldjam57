924fe3e5d683b2099735d330ccbfaee2a5a6a566
Arso <mr.sen4eg@gmail.com>
2025-04-07 16:05:07 +0200

    intermidiate

--- Assets/Tube/TubeScript.cs
+++ Assets/Tube/TubeScript.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
 
-public class TubeScripte : MonoBehaviour
+[System.Serializable]
+public class TubeLevelInfo
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int levelIndex;
+    public Material[] insideTextures;
+    public float levelHeight;
+    public Color tubeColor;
+    public Material textureLessMaterial;  // Material without texture
+}
+
+public class TubeScript : MonoBehaviour
+{
+    [SerializeField] private TubeLevelInfo[] levelInfoArray;
+    [SerializeField] private MeshFilter meshFilter;
+    private float tubeHeight;
+    private Mesh tubeMesh;
+
     void Start()
     {
-        
+        if (meshFilter != null)
+        {
+            tubeMesh = meshFilter.mesh;
+            tubeHeight = tubeMesh.bounds.size.y;
+        }
+
+        BuildTube();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void BuildTube()
+    {
+        if (levelInfoArray != null && levelInfoArray.Length > 0)
+        {
+            for (int i = 0; i < levelInfoArray.Length; i++)
+            {
+                var levelInfo = levelInfoArray[i];
+                ApplyLevelInfo(levelInfo, i);
+            }
+        }
+    }
+
+    private void ApplyLevelInfo(TubeLevelInfo levelInfo, int index)
+    {
+        if (levelInfo.insideTextures != null && levelInfo.insideTextures.Length > 0 && meshFilter != null)
+        {
+            var meshRenderer = meshFilter.GetComponent<MeshRenderer>();
+            if (meshRenderer != null)
+            {
+                meshRenderer.material = levelInfo.insideTextures[0];  // Apply first texture for simplicity
+            }
+        }
+
+        if (levelInfo.tubeColor != null)
+        {
+            var meshRenderer = meshFilter.GetComponent<MeshRenderer>();
+            if (meshRenderer != null)
+            {
+                meshRenderer.material.color = levelInfo.tubeColor;
+            }
+        }
+
+        if (levelInfo.textureLessMaterial != null)
+        {
+            var meshRenderer = meshFilter.GetComponent<MeshRenderer>();
+            if (meshRenderer != null)
+            {
+                meshRenderer.material = levelInfo.textureLessMaterial;  // Use the texture-less material
+            }
+        }
     }
 }
